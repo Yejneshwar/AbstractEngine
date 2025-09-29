95edaa14e2b865787b03d439e974c05f743b108f
Yejneshwar Sivamoorthy <yejneshwar@gmail.com>
2025-09-29 19:43:36 +0530

    fix: compute shader local size

--- Resources/ComputeShaders/JFAPass.glsl
+++ Resources/ComputeShaders/JFAPass.glsl
@@ -6,7 +6,7 @@ layout(binding = 1, rgba32f) writeonly uniform image2D outTexture;
 
 layout(location = 2) uniform int step;
 
-layout(local_size_x = 16, local_size_y = 16, local_size_z = 1) in;
+layout(local_size_x = 8, local_size_y = 8, local_size_z = 1) in;
 
 void main() {
     ivec2 gid = ivec2(gl_GlobalInvocationID.xy);
